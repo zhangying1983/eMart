44440dde1c1ca678c67f8c2a7bb8b6979aa935ff
Ying Zhang <dlyzhang@cn.ibm.com>
2020-06-15 19:37:38 +0800

    jmeter

--- eMart/src/app/register-item/register-item.component.ts
+++ eMart/src/app/register-item/register-item.component.ts
@@ -6,11 +6,11 @@ import { FormBuilder } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: 'app-register-item',
+  templateUrl: './register-item.component.html',
+  styleUrls: ['./register-item.component.css']
 })
-export class SignupComponent implements OnInit {
+export class RegisterItemComponent implements OnInit {
 
 
   errors = { 
@@ -26,31 +26,6 @@ export class SignupComponent implements OnInit {
   }
   // TODO
   signUp(): boolean {
-    this.errors.username = "";
-    this.errors.confirmPassword = "";
-    this.errors.hasError = false
-    if(this.password.value !== this.confirmPassword.value){
-      this.errors.confirmPassword = "password not match."
-      this.errors.hasError = true
-    }
-    this.http.get('api/user/' + this.username.value + "/exist").toPromise().then((result: any) => {
-      if(!result){
-        this.errors.username = "user has already exist."
-        this.errors.hasError = true
-      }
-      if(this.errors.hasError){
-        return false
-      }
-      this.http.post('/api/user/buyer', {
-        "username": this.username.value,
-        "password": this.password.value,
-        "email": this.email.value,
-        "mobileNumber": this.mobileNumber.value
-      })
-        .toPromise().then((result: any) => {
-          this.router.navigate(["/login"])
-        });
-    })
     return false;
   }
 }
